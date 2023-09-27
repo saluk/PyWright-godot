8fdd1fe3f20f3e7866739d35bf457116d5740082
saluk <saluk64007@gmail.com>
2023-09-27 14:11:25 -0700

    Shrink character width of spaces in textboxes to apprach equivalence with pywright text wraps

--- System/UI/Textbox.gd
+++ System/UI/Textbox.gd
@@ -205,6 +205,8 @@ func _ready():
 	var font = DynamicFont.new()
 	font.font_data = load(font_path)
 	font.size = 10
+	font.set_spacing(DynamicFont.SPACING_SPACE, -1)
+	font.use_filter = true
 	
 	tb_timer = get_node(tb_timer)
 	tb_timer.one_shot = true
