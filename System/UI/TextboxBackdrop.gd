cfcd9382ed4d2c340bdcb426c01521ecd56bbc96
saluk <saluk64007@gmail.com>
2024-08-31 09:13:20 -0700

    Backdrop sometimes doesn't have a parent, somehow

--- System/UI/TextboxBackdrop.gd
+++ System/UI/TextboxBackdrop.gd
@@ -8,8 +8,8 @@ func _ready():
 		var PWSpriteC = load("res://System/Graphics/PWSprite.gd")
 		var sprite = PWSpriteC.new()
 		sprite.name = "PWSprite:"+bg
-		sprite.pivot_center = false
-		sprite.load_animation("art/"+bg+".png", get_parent().main.stack.scripts[-1].root_path)
+		#sprite.pivot_center = false
+		sprite.load_animation("art/"+bg+".png", Commands.main.stack.scripts[-1].root_path)
 		add_child(sprite)
 		move_child(sprite, 0)
 		get_node("Textbox2").queue_free()
