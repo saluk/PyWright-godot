3a720318b590b7ad2944edeac219d58554ba8ae2
saluk <saluk64007@gmail.com>
2025-03-13 21:43:58 -0700

    Fix signalutils

--- System/SignalUtils.gd
+++ System/SignalUtils.gd
@@ -5,8 +5,8 @@ class_name SignalUtils
 static func remove_all(object:Object):
 	if not is_instance_valid(object):
 		return
-	for signal_name in object.get_signal_list():
-		signal_name = signal_name["name"]
+	for signal_dict in object.get_signal_list():
+		var signal_name = signal_dict["name"]
 		for connection in object.get_signal_connection_list(signal_name):
 			if object.is_connected(connection["signal"], Callable(connection["target"], connection["method"])):
 				print("disconnect:", connection)
